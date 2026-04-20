955d64e093e7e50a4a30069cfc530c19335117b4
faken <ctgpghosts@gmail.com>
2026-04-20 14:41:09 -0400

    created functions

--- src/CampusCompass.cpp
+++ src/CampusCompass.cpp
@@ -20,3 +20,77 @@ bool CampusCompass::ParseCommand(const string &command) {
 
     return is_valid;
 }
+// Insert a new student
+bool CampusCompass::insert(const std::string& STUDENT_NAME,
+                           const std::string& STUDENT_ID,
+                           const std::string& RESIDENCE_LOCATION_ID,
+                           const std::vector<std::string>& CLASSCODES) {
+    // TODO
+
+    return false;
+}
+
+// Remove a student
+bool CampusCompass::remove(const std::string& STUDENT_ID) {
+    // TODO
+    return false;
+}
+
+// Drop a specific class for a student
+bool CampusCompass::dropClass(const std::string& STUDENT_ID,
+                             const std::string& CLASSCODE) {
+    // TODO
+    return false;
+}
+
+// Replace one class with another
+bool CampusCompass::replaceClass(const std::string& STUDENT_ID,
+                                const std::string& CLASSCODE_1,
+                                const std::string& CLASSCODE_2) {
+    // TODO
+    return false;
+}
+
+// Remove a class entirely from the system
+bool CampusCompass::removeClass(const std::string& CLASSCODE) {
+    // TODO
+    return false;
+}
+
+// Toggle edge closures for a location
+bool CampusCompass::toggleEdgesClosure(const std::string& N,
+                                       const std::vector<std::string>& LOCATION_IDS) {
+    // TODO
+    return false;
+}
+
+// Check if an edge is open/closed
+bool CampusCompass::checkEdgeStatus(const std::string& LOCATION_ID_X,
+                                    const std::string& LOCATION_ID_Y) {
+    // TODO
+    return false;
+}
+
+// Check connectivity between two locations
+bool CampusCompass::isConnected(const std::string& LOCATION_ID_1,
+                                const std::string& LOCATION_ID_2) {
+    // TODO
+    return false;
+}
+
+// Print shortest path edges for a student
+void CampusCompass::printShortestEdges(const std::string& STUDENT_ID) {
+    // TODO
+}
+
+// Print student’s zone
+void CampusCompass::printStudentZone(const std::string& STUDENT_ID) {
+    // TODO
+}
+
+// Verify if student's schedule is valid
+bool CampusCompass::verifySchedule(const std::string& STUDENT_ID) {
+    // TODO
+    return false;
+}
+
--- src/CampusCompass.h
+++ src/CampusCompass.h
@@ -1,15 +1,40 @@
 #pragma once
+#include <set>
 #include <string>
+#include <unordered_map>
+#include <vector>
 
 using namespace std;
+
+class Student {
+private:
+    string STUDENT_NAME;
+    string STUDENT_ID;
+    string RESIDENCE_LOCATION_ID;
+    vector<string> CLASSCODES;
+    unordered_map<pair<string,string>, string> GRAPH;
+};
 
 class CampusCompass {
 private:
     // Think about what member variables you need to initialize
     // perhaps some graph representation?
+    unordered_map<string, Student> students;
+    // ID, Name, Residence ID, Class Codes
 public:
     // Think about what helper functions you will need in the algorithm
     CampusCompass(); // constructor
     bool ParseCSV(const string &edges_filepath, const string &classes_filepath);
     bool ParseCommand(const string &command);
+    bool insert(const string& STUDENT_NAME, const string& STUDENT_ID, const string& RESIDENCE_LOCATION_ID, const vector<string>& CLASSCODES);
+    bool remove(const string& STUDENT_ID);
+    bool dropClass(const string& STUDENT_ID, const string& CLASSCODE);
+    bool replaceClass(const string& STUDENT_ID, const string& CLASSCODE_1, const string& CLASSCODE_2);
+    bool removeClass(const string& CLASSCODE);
+    bool toggleEdgesClosure(const string& N, const vector<string>& LOCATION_IDS);
+    bool checkEdgeStatus(const string& LOCATION_ID_X,const string& LOCATION_ID_Y);
+    bool isConnected(const string& LOCATION_ID_1,const string& LOCATION_ID_2);
+    void printShortestEdges(const string& STUDENT_ID);
+    void printStudentZone(const string& STUDENT_ID);
+    bool verifySchedule(const string& STUDENT_ID);
 };
